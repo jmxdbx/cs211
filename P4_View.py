83018fb8e72a33046bd62d74cb38f0538243e162
jmxdbx <joelbny@gmail.com>
2018-11-01 01:45:54 -0400

    Update P4_View.py

--- P4_View.py
+++ P4_View.py
@@ -1,7 +1,5 @@
 """
 P4_View.py. CIS 211 Spring 2016 Project 4 Final. 2016-05-28.
-Author: Joel Berry. Modified Anthony Hornof's solution code for this module.
-Credits: Project spec, starter code by Anthony Hornof, 2016-04-08.
 
 Basic Model-View-Controller fire-fighting simulation.
 """
